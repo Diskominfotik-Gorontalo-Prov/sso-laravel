616757d1376f683adade0748215c87e7fa76f94a
alfandy <brayandyi@gmail.com>
2024-07-29 18:25:27 +0800

    pervbiakan aptika route config publish

--- src/Providers/SSOServiceProvider.php
+++ src/Providers/SSOServiceProvider.php
@@ -6,13 +6,21 @@ use Illuminate\Support\ServiceProvider;
 
 class SSOServiceProvider extends ServiceProvider
 {
+    
     public function boot()
     {
         $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
+
+        $this->publishes([
+            __DIR__ . '/../config/aptika-sso.php' => config_path('aptika-sso.php'),
+        ], 'config');
     }
 
     public function register()
     {
-        //
+        $this->mergeConfigFrom(
+            __DIR__ . '/../config/aptika-sso.php',
+            'aptika-sso'
+        );
     }
 }
